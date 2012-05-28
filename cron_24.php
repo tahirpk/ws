fc1532cee62d5459627465823d7d6d959bd6d882
Tahir Khan <tahirpk@gmail.com>
2012-05-28 21:10:04 +0500

    tahir commit

--- cron_24.php
+++ cron_24.php
@@ -59,16 +59,15 @@
 		text-decoration: none;
 		}
 		</style>';
-        
-        //cronJobStatus=1 for the 24:00 
-      
+        //$sql='select wb.id,wb.url,webId,pg.pageUrl,pageTitle,pageKeywords,pageContent,reportCheckStatus,reportStatus 
+          //  from websites as wb, pages as pg where wb.id=pg.webId ';
         $sqlurl='select id,url from websites where status=1';
         $sqlurl=mysql_query($sqlurl) or die(mysql_error());
         $num_rows = mysql_num_rows($sqlurl);
         $table=$htmlStyle.'<table class="brd" width="100%" border="0" cellspacing="0" cellpadding="0"><tr class="tr_head bottom15"><td class="top25" colspan="6" align="center">Websites Urls Reports To Admin:</td></tr>';
         while ($row = mysql_fetch_array($sqlurl)) {
-           $sqlweb='select webId,pg.pageUrl,pageTitle,pageKeywords,pageContent,reportCheckStatus,reportStatus 
-           from pages as pg where status=1 and cronJobStatus=1 and webId='.$row['id'];
+           $sqlweb='select webId,pg.pageUrl,pageTitle,pageKeywords,pageContent,reportCheckStatus,reportStatus,cronJobStatus
+           from pages as pg where status=1 and cronJobStatus!=1 and webId='.$row['id'];
         $table.='<tr class="tr_head "><td class="top25">Main Url:</td><td colspan="4" class="top25">'. $row["url"].'</td></tr>';
         $table.='<tr class="tr_sbhead "><td>Page Url</td><td>Page Title</td><td>Keywords</td><td>Descriptions</td></tr>';            
            $sqlweb= mysql_query($sqlweb);
@@ -77,20 +76,11 @@
            {
                 while($prow=  mysql_fetch_array($sqlweb))
                 {
-                    if($row['id']==$prow['webId'])
-                    {
-                        if($prow["reportStatus"]==0)
-                            $rpstatus='None';
-                        if($prow["reportStatus"]==1)
-                            $rpstatus='Only Title';
-                        if($prow["reportStatus"]==2)
-                            $rpstatus='Title and Keywords';
-                        if($prow["reportStatus"]==3)
-                            $rpstatus='Title,Keywords and Descriptions';
-                        $table.='<tr><td>'.$prow["pageUrl"] .'</td><td>'.$prow["pageTitle"] .'</td><td>'.$prow["pageKeywords"] .'</td><td>'.$prow["pageContent"].'</td></tr>';
+                    
+                     
+                          $table.='<tr><td>'.$prow["pageUrl"].'</td><td>'.$prow["pageTitle"] .'</td><td>'.$prow["pageKeywords"] .'</td><td>'.$prow["pageContent"].'</td></tr>';
 
-                    }
-                
+                    
                  }
            }
            else
@@ -104,10 +94,10 @@
         }
        $table.='</table>';
        
-    //   echo $table;
+       echo $table;
        
-        $to             = 'tahirpk@gmail.com,chris.adler@gmail.com';
-        $from         = 'tahirkhan3311@yahoo.com';
+        $to		  = 'tahirpk@gmail.com,chris.adler@gmail.com';
+        $from         = 'tahirpk@gmail.com';
         $subject      = 'Valid URLS Metatags.';
         //$Mail_header  = "Content-type: text/html\n";
         $Mail_header  = "MIME-Version: 1.0\r\n";
